bfa1a1c2c0bce45edac8bee6446008b0648ed686
12dharani <chittepureddydharani@gmail.com>
2023-12-08 18:53:32 +0530

    todays

---
+++ readfilesync.js
@@ -0,0 +1,15 @@
+const fs= require("fs");
+
+console.log("script started");
+
+try{
+
+    const data = fs.readFileSync("test.txt");
+    console.log("file read completed")
+}
+catch(err){
+    console.log(err)
+}
+
+console.log("script stopped")
+
